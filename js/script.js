4394c2c099e5e8c80f212bf53eb623bfa286c318
Gani Widian Nugraha <gani.widiannugraha@gmail.com>
2022-09-10 22:27:37 +0700

    final project

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var typing = new Typed('.typing', {
+  strings: ['Web Designer', 'Freelancer', 'Graphics Designer', 'Web Developer'],
+  typeSpeed: 150,
+  backSpeed: 40,
+  loop: true,
+});
+alert(hello);
+prompt(apakabar);
